02c13c1e315d542bc45c120f06f8a09db8d35e94
Brendan <bgmcmullen@comcast.net>
2024-02-21 20:55:19 -0500

    challenge 1

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -9,13 +9,17 @@ Write a function named raisedToTheThird that takes in an array of numbers and re
 ------------------------------------------------------------------------------------------------ */
 
 const raisedToTheThird = (arr) => {
-  // Solution code here...
+  let outputArr = [];
+  arr.forEach((element) => {
+    outputArr.push(Math.pow(element, 3));
+  })
+  return outputArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
 
-Write a function named addOne that, given an array of numbers, uses map to return a new array with each value simply incremented by 1. 
+Write a function named addOne that, given an array of numbers, uses map to return a new array with each value simply incremented by 1.
 ------------------------------------------------------------------------------------------------ */
 
 const addOne = (arr) => {
