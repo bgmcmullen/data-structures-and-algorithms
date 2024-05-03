61d2579676ed2d4d3aa6f4e2be739012575ae0e5
Brendan <bgmcmullen@comcast.net>
2024-05-03 19:12:18 -0400

    Completed Linked List

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,45 @@
 'use strict';
 
-class LinkedList {
-
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
+
+class LinkedList {
+  contructor() {
+    this.head = null;
+  }
+
+  insert(value) {
+    let newHead = new Node(value);
+    newHead.next = this.head;
+    this.head = newHead;
+
+  }
+
+  includes(searchValue) {
+    let current = this.head;
+
+    while (current) {
+      if (current.value === searchValue)
+        return true;
+      current = current.next;
+    }
+    return false;
+  }
+  toString() {
+    let current = this.head;
+    let listString = "";
+    while (current) {
+      listString = listString + `{ ${current.value} } -> `;
+      current = current.next;
+    }
+    listString =listString + 'NULL';
+    return listString;
+  }
+}
+
 
 module.exports = LinkedList;
